8991961ffa5fd6bbd0d863ba0bcebd8d606aa579
osuzuki <osuzuki@plegineer.com>
2014-03-02 04:29:40 +0900

    ゲームオーバー処理追加

--- NGGThreader/NGGViewController.m
+++ NGGThreader/NGGViewController.m
@@ -10,12 +10,20 @@
 
 #define SCROLL_SPEED 5
 
+typedef NS_ENUM(NSInteger, NGGViewStatus) {
+    NGGVIewStatusNone = 0,
+    NGGVIewStatusAlive = 1,
+    NGGVIewStatusGameOver = 2,
+    NGGVIewStatusStandby = 3,
+};
+
 @interface NGGViewController ()<UICollisionBehaviorDelegate>
 @property (nonatomic, strong) UIImageView *ballImageView;
 @property (nonatomic, strong) UIImageView *groundImageView;
 @property (nonatomic, strong) UIDynamicAnimator *animator;
 @property (nonatomic, strong) UIPushBehavior *floatUpBehavior;
 @property (nonatomic, strong) NSMutableArray *sceneries;
+@property (nonatomic, assign) NGGViewStatus viewStatus;
 @end
 
 @implementation NGGViewController
@@ -23,8 +31,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	
-    self.sceneries = [[NSMutableArray alloc] init];
     
     UIView *bgView = [[UIView alloc] initWithFrame:self.view.frame];
     bgView.backgroundColor = [UIColor colorWithRed:0 green:150.0f/255.0f blue:255.0f alpha:1.0f];
@@ -41,33 +47,11 @@
     UIImageView *groundImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"ground"]];
     groundImageView.frame = CGRectMake(0, [self displaySize].height-40, 640, 40);
     [self.view addSubview:groundImageView];
-    [self.sceneries addObject:groundImageView];
     self.groundImageView = groundImageView;
     
-    UIDynamicAnimator *animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
-    UIGravityBehavior *gravityBehvior = [[UIGravityBehavior alloc] initWithItems:@[ballImageView]];
-    [animator addBehavior:gravityBehvior];
-    
-    UICollisionBehavior *collisionBehavior = [[UICollisionBehavior alloc] initWithItems:@[ballImageView]];
-    collisionBehavior.translatesReferenceBoundsIntoBoundary = YES;
-    collisionBehavior.collisionDelegate = self;
-    [collisionBehavior addBoundaryWithIdentifier:@"ground" fromPoint:CGPointMake(0, groundImageView.frame.origin.y) toPoint:CGPointMake(groundImageView.frame.size.width, groundImageView.frame.origin.y)];
-    [animator addBehavior:collisionBehavior];
-    
-    UIPushBehavior *floatUpBeahavior = [[UIPushBehavior alloc] initWithItems:@[self.ballImageView] mode:UIPushBehaviorModeInstantaneous];
-    floatUpBeahavior.pushDirection = CGVectorMake(0, -0.5);
-    floatUpBeahavior.active = NO;
-    [animator addBehavior:floatUpBeahavior];
-    self.floatUpBehavior = floatUpBeahavior;
-    
-    self.animator = animator;
-    
-    UIDynamicBehavior *scrollBehavior = [[UIDynamicBehavior alloc] init];
-    scrollBehavior.action = ^ {
-        [self updateViews];
-        
-    };
-    [self.animator addBehavior:scrollBehavior];
+    [self resetViewsAndAnimator];
+    [self setBehaviors];
+    _viewStatus = NGGVIewStatusAlive;
 }
 
 - (void)didReceiveMemoryWarning
@@ -84,6 +68,33 @@
     return [[UIScreen mainScreen] bounds].size;
 }
 
+- (void)setBehaviors
+{
+    UIDynamicAnimator *animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+    UIGravityBehavior *gravityBehvior = [[UIGravityBehavior alloc] initWithItems:@[self.ballImageView]];
+    [animator addBehavior:gravityBehvior];
+    
+    UICollisionBehavior *collisionBehavior = [[UICollisionBehavior alloc] initWithItems:@[self.ballImageView]];
+    collisionBehavior.translatesReferenceBoundsIntoBoundary = YES;
+    collisionBehavior.collisionDelegate = self;
+    [collisionBehavior addBoundaryWithIdentifier:@"ground" fromPoint:CGPointMake(0, self.groundImageView.frame.origin.y) toPoint:CGPointMake(self.groundImageView.frame.size.width, self.groundImageView.frame.origin.y)];
+    [animator addBehavior:collisionBehavior];
+    
+    UIPushBehavior *floatUpBeahavior = [[UIPushBehavior alloc] initWithItems:@[self.ballImageView] mode:UIPushBehaviorModeInstantaneous];
+    floatUpBeahavior.pushDirection = CGVectorMake(0, -0.5);
+    floatUpBeahavior.active = NO;
+    [animator addBehavior:floatUpBeahavior];
+    self.floatUpBehavior = floatUpBeahavior;
+    
+    UIDynamicBehavior *scrollBehavior = [[UIDynamicBehavior alloc] init];
+    scrollBehavior.action = ^ {
+        [self updateViews];
+    };
+    [animator addBehavior:scrollBehavior];
+    
+    self.animator = animator;
+}
+
 - (void)updateViews
 {
 //    NSTimeInterval interval = self.animator.elapsedTime;
@@ -94,7 +105,6 @@
     
     if (self.groundImageView.frame.origin.x <= -[self displaySize].width) {
         self.groundImageView.center = CGPointMake([self displaySize].width, self.groundImageView.center.y);
-        NSLog(@"new! bg! %@",NSStringFromCGRect(self.groundImageView.frame));
         
         for(UIView *scenary in [self.sceneries reverseObjectEnumerator]){
             if(scenary.frame.origin.x+scenary.frame.size.width < 0){
@@ -103,13 +113,24 @@
             }
         }
         
-        [self addBuildings];
+        [self setBuildingViews];
+    }
+    
+    //衝突判定
+    for (UIView *scenary  in self.sceneries) {
+        if(CGRectIntersectsRect(self.ballImageView.frame, scenary.frame)){
+            NSLog(@"Game Over!");
+            [self setGameOverLabel];
+            [self.animator removeAllBehaviors];
+            _viewStatus = NGGVIewStatusGameOver;
+            break;
+        }
     }
     
 //    NSLog(@"action %f x:%f",interval,self.groundImageView.center.x);
 }
 
-- (void)addBuildings
+- (void)setBuildingViews
 {
     CGFloat interSpace = 80;
     NSInteger offset = arc4random()%200-100;
@@ -128,12 +149,43 @@
     [self.sceneries addObject:downSideBuildingImageView];
 }
 
+- (void)setGameOverLabel
+{
+    UILabel *gameOverLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 100, [self displaySize].width, 200)];
+    gameOverLabel.backgroundColor = [UIColor clearColor];
+    gameOverLabel.textAlignment = NSTextAlignmentCenter;
+    gameOverLabel.textColor = [UIColor whiteColor];
+    gameOverLabel.text = @"GAME OVER";
+    gameOverLabel.font = [UIFont boldSystemFontOfSize:32];
+    [self.view addSubview:gameOverLabel];
+    [self.sceneries addObject:gameOverLabel];//まとめてremoveFromSuperviewしてもらうため
+}
+
+- (void)resetViewsAndAnimator
+{
+    [self.sceneries makeObjectsPerformSelector:@selector(removeFromSuperview)];
+    self.sceneries = [[NSMutableArray alloc] init];
+    
+    [self.animator removeAllBehaviors];
+    
+    self.ballImageView.center = CGPointMake([self displaySize].width/2, [self displaySize].height/2);
+    self.groundImageView.frame = CGRectMake(0, [self displaySize].height-40, 640, 40);
+    [self.view insertSubview:self.groundImageView belowSubview:self.ballImageView];
+    [self.sceneries addObject:self.groundImageView];
+}
+
 #pragma mark - Touch Event
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
 {
     NSLog(@"%@",NSStringFromSelector(_cmd));
-    self.floatUpBehavior.active = YES;
+    if (_viewStatus == NGGVIewStatusAlive) {
+        self.floatUpBehavior.active = YES;
+    }else if(_viewStatus == NGGVIewStatusGameOver){
+        [self resetViewsAndAnimator];
+        [self setBehaviors];
+        _viewStatus = NGGVIewStatusAlive;
+    }
 }
 
 #pragma mark - UICollisionBehaviorDelegate
